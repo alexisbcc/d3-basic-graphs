a8aefb59e4c361ec06bd945196fb061d707c592d
Alexis Crespo <alexis-2cab@hotmail.com>
2021-01-28 16:50:21 -0500

    Responsive line chart

--- src/components/line.jsx
+++ src/components/line.jsx
@@ -9,19 +9,37 @@ import {
 } from "d3";
 import "./line.css";
 
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect);
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
+
 function Line() {
   const [data, setData] = useState([21, 5, 14, 33, 4, 50, 22, 14, 43, 32]);
   const svgRef = useRef();
+  const wrapperRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
 
-  const graphWidth = 500;
   const graphHeight = 150;
 
   useEffect(() => {
     const svg = select(svgRef.current);
-
+    if (!dimensions) return;
     const xScale = scaleLinear()
       .domain([0, data.length - 1])
-      .range([0, graphWidth]);
+      .range([0, dimensions.width]);
     const yScale = scaleLinear().domain([0, 75]).range([graphHeight, 0]);
     const xAxis = axisBottom(xScale)
       .ticks(data.length)
@@ -49,10 +67,10 @@ function Line() {
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 3);
-  }, [data]);
+  }, [data, dimensions]);
 
   return (
-    <React.Fragment>
+    <div ref={wrapperRef} className="line-container">
       <svg ref={svgRef}>
         <g className="x-axis" />
         <g className="y-axis" />
@@ -61,7 +79,7 @@ function Line() {
       <button onClick={() => setData(data.map(value => value+2))}>
         Update data
       </button> */}
-    </React.Fragment>
+    </div>
   );
 }
 
