b504411d38fc0b89a2d338101533955464b22427
Alexis Crespo <alexis-2cab@hotmail.com>
2021-02-01 19:45:07 -0500

    Tooltip added

--- src/components/line.jsx
+++ src/components/line.jsx
@@ -7,6 +7,7 @@ import {
   curveCardinal,
   scaleLinear,
   pointer,
+  bisector
 } from "d3";
 import { transition } from "d3-transition";
 import "./line.css";
@@ -29,7 +30,22 @@ const useResizeObserver = (ref) => {
 };
 
 function Line() {
-  const [data, setData] = useState([21, 5, 14, 33, 4, 50, 22, 14, 43, 32]);
+  const [data, setData] = useState([
+    { x: 0, y: 21 },
+    { x: 1, y: 14 },
+    { x: 2, y: 15 },
+    { x: 3, y: 4 },
+    { x: 4, y: 1 },
+    { x: 5, y: 43 },
+    { x: 6, y: 50 },
+    { x: 7, y: 51 },
+    { x: 8, y: 18 },
+    { x: 9, y: 33 },
+    { x: 10, y: 38 },
+    { x: 11, y: 3 },
+    { x: 12, y: 25 },
+    { x: 13, y: 47 },
+  ]);
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
@@ -44,13 +60,14 @@ function Line() {
     const xScale = scaleLinear()
       .domain([0, data.length - 1])
       .range([0, dimensions.width]);
-    const yScale = scaleLinear().domain([0, 75]).range([graphHeight, 0]);
+    const yScale = scaleLinear().domain([0, 60]).range([graphHeight, 0]);
     const xAxis = axisBottom(xScale)
       .ticks(data.length)
-      .tickFormat((index) => index + 1);
-    const yAxis = axisLeft(yScale);
+      .tickFormat((index) => index);
+    const yAxis = axisLeft(yScale)
+    .ticks(5);
 
-    // Scales join
+    // Axis
     svg
       .select(".x-axis")
       .style("transform", `translateY(${graphHeight}px)`)
@@ -59,9 +76,8 @@ function Line() {
 
     // Generates d attr of path element
     const myLine = line()
-      .x((value, index) => xScale(index))
-      .y(yScale)
-      .curve(curveCardinal);
+      .x((value) => xScale(value.x))
+      .y((value) => yScale(value.y));
 
     svg
       .selectAll(".line")
@@ -76,28 +92,44 @@ function Line() {
     // Tooltip
     const tooltip = svg.select(".line-tooltip").style("display", "none");
 
-    svg.selectAll("path").on("touchmove mousemove", (event, d) => {
-      const coords = pointer(event);
+    svg.on("touchmove mousemove", (event) => {
+      // Find the value of the closest point
+      // Extract the data from the path element
+      const lineData = svg.select(".line").data()[0];
+      // Convert the corrdinate from pointer (range - svg dimensions) to domain
+      const domainXCoordinate = xScale.invert(pointer(event)[0]);
+      // find the index to the closest value
+      const coords = bisector(d => d.x).center(lineData, domainXCoordinate);
 
       tooltip
-        .attr("transform", `translate(${coords[0]},${coords[1]})`)
+        .attr(
+          "transform",
+          `translate(${xScale(lineData[coords].x)},${yScale(lineData[coords].y)})`
+        )
         .style("display", null)
-        .selectAll("text")
-        .data([`${coords[1]},${coords[0]}`])
-        .join("text")
-        .text(d)
-        .attr("class", "tooltip")
+        .style("pointer-events", "none")
+        .style("font", "10px sans-serif");
+
+      const path = tooltip
         .selectAll("path")
         .data([null])
         .join("path")
         .attr("fill", "white")
-        .attr("stroke", "black")
-        .attr(
-          "d",
-          `M${-50 / 2 - 10},5H-5l5,-5l5,5H${50 / 2 + 10}v${40 + 20}h-${
-            50 + 20
-          }z`
-        );
+        .attr("stroke", "black");
+
+      const text = tooltip
+        .selectAll("text")
+        .data([lineData[coords].y])
+        .join("text")
+        .text((d) => d);
+
+      const { x, y, width: w, height: h } = text.node().getBBox();
+
+      text.attr("transform", `translate(${-w / 2},${15 - y})`);
+      path.attr(
+        "d",
+        `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`
+      );
     });
 
     svg.on("touchend mouseleave", () => tooltip.style("display", "none"));
